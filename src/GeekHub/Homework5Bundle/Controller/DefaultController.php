d4c081590cfc94e09b9e3454a4357319ade0ec54
NatashaLebed <Natasha.lebed@gmail.com>
2013-11-14 09:45:06 +0200

    twig filtres and functions, render, create contact task actions

--- src/GeekHub/Homework5Bundle/Controller/DefaultController.php
+++ src/GeekHub/Homework5Bundle/Controller/DefaultController.php
@@ -10,4 +10,27 @@ class DefaultController extends Controller
     {
         return $this->render('GeekHubHomework5Bundle:Default:index.html.twig');
     }
+
+    public function taskAction()
+    {
+         $tasks = array ('Read and understand Twig',
+                       'Reed and use Assetic',
+                       'Research about Bootstrap',
+                       'Create base template of your application',
+                       'Use assetic to add css and js to your templates',
+                      );
+        return $this->render('GeekHubHomework5Bundle:Default:task.html.twig', array('tasks' => $tasks));
+    }
+
+    public function contactAction()
+    {
+        $phone = '098 111 11 11';
+        return $this->render('GeekHubHomework5Bundle:Default:contact.html.twig', array('phone' => $phone));
+    }
+
+    public function renderTaskAction()
+    {
+        return $this->render('GeekHubHomework5Bundle:Default:renderTask.html.twig');
+    }
+
 }
